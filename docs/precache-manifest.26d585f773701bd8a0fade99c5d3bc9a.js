e4a701029452b3dd88d161cb74f44a1cc88bdc75
LuisP <luisjavierpolancomorel@gmail.com>
2021-03-08 19:49:45 -0400

    docs folder created

---
+++ docs/precache-manifest.26d585f773701bd8a0fade99c5d3bc9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f674f489b866c6f37d306091c977f735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a1404a55f9cddfb60db",
+    "url": "/static/css/main.45068df5.chunk.css"
+  },
+  {
+    "revision": "f57640abf0616ae7bd2f",
+    "url": "/static/js/2.d509916f.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.d509916f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1404a55f9cddfb60db",
+    "url": "/static/js/main.eea51e67.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
